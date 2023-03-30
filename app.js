b590f03ce1fb241cc697664a1135dabce480bfca
Fredy Velasquez <macacoloco696@gmail.com>
2023-03-30 17:53:38 -0600

    created database configs

--- app.js
+++ app.js
@@ -3,9 +3,16 @@ const app = express();
 
 const path = require('path');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 
-dotenv.config({path: './config.env'})
+dotenv.config({path: './config.env'});
+
+//connecting to mongodb database
+mongoose.connect(proces.env.DATABASE_LOCAL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+});
 
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'ejs');
